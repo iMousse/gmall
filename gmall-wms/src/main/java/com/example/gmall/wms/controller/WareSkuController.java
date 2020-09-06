2f533abe0b6d7a51fa8c10a5a37b6e1ec11a4b72
mousse <958860184@qq.com>
2020-09-06 14:43:00 +0800

    查询库存信息

--- gmall-wms/src/main/java/com/example/gmall/wms/controller/WareSkuController.java
+++ gmall-wms/src/main/java/com/example/gmall/wms/controller/WareSkuController.java
@@ -1,12 +1,14 @@
 package com.example.gmall.wms.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
 import com.atguigu.core.bean.PageVo;
 import com.atguigu.core.bean.QueryCondition;
 import com.atguigu.core.bean.Resp;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +35,16 @@ public class WareSkuController {
     @Autowired
     private WareSkuService wareSkuService;
 
+
+    @GetMapping("{skuId}")
+    public Resp<List<WareSkuEntity>> queryWareSkusBySkuId(@PathVariable("skuId") Long skuId) {
+
+        List<WareSkuEntity> list = this.wareSkuService.list(new QueryWrapper<WareSkuEntity>().eq("sku_id", skuId));
+
+        return Resp.ok(list);
+    }
+
+
     /**
      * 列表
      */
@@ -40,6 +52,7 @@ public class WareSkuController {
     @GetMapping("/list")
     @PreAuthorize("hasAuthority('wms:waresku:list')")
     public Resp<PageVo> list(QueryCondition queryCondition) {
+
         PageVo page = wareSkuService.queryPage(queryCondition);
 
         return Resp.ok(page);
