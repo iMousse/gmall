f0b54b99ae56499a905a17bb7e42fce30b9957cc
mousse <958860184@qq.com>
2020-11-15 11:18:17 +0800

    订单确认页的异步编排

--- gmall-order/src/main/java/com/example/gmallorder/service/impl/OrderServiceImpl.java
+++ gmall-order/src/main/java/com/example/gmallorder/service/impl/OrderServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.gmallorder.service.impl;
 import com.atguigu.core.bean.UserInfo;
 import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.example.gmall.cart.vo.Cart;
-import com.example.gmall.oms.api.GmallOmsApi;
 import com.example.gmall.pms.entity.SkuInfoEntity;
 import com.example.gmall.pms.entity.SkuSaleAttrValueEntity;
 import com.example.gmall.ums.entity.MemberEntity;
@@ -19,7 +18,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
 
 @Service
@@ -39,6 +41,8 @@ public class OrderServiceImpl implements OrderService {
     private GmallSmsClient smsClient;
     @Autowired
     private GmallWmsClient wmsClient;
+    @Autowired
+    private ThreadPoolExecutor threadPoolExecutor;
 
     @Override
     public OrderConfirmVO confirm() {
@@ -49,44 +53,77 @@ public class OrderServiceImpl implements OrderService {
             return null;
         }
 
+
+        //收集异步请求的集合
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+
         //获取用户的id查询收货地址列表
-        List<MemberReceiveAddressEntity> addressEntities = this.umsClient.queryAddressByUserId(userId).getData();
-        orderConfirm.setAddresses(addressEntities);
+        CompletableFuture<Void> addressCompletableFuture = CompletableFuture.runAsync(() -> {
+            List<MemberReceiveAddressEntity> addressEntities = this.umsClient.queryAddressByUserId(userId).getData();
+            orderConfirm.setAddresses(addressEntities);
+        }, threadPoolExecutor);
+        futures.add(addressCompletableFuture);
 
         //获取购物车中选中的商品信息
-        List<Cart> carts = this.cartClient.queryCartByUserId(userId).getData();
-        if (CollectionUtils.isEmpty(carts)) {
-            throw new RuntimeException("请勾选购物车商品");
-        }
+        //线程池异步请求返回参数后设置
+        CompletableFuture<Void> skuCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            //获取购物车中选中的商品信息
+            List<Cart> carts = this.cartClient.queryCartByUserId(userId).getData();
+            if (CollectionUtils.isEmpty(carts)) {
+                throw new RuntimeException("请勾选购物车商品");
+            }
+            return carts;
+        }, threadPoolExecutor).thenAcceptAsync(carts -> {
+            orderConfirm.setOrderItems(carts.stream().map(cart -> {
+                OrderItemVO orderItemVO = new OrderItemVO();
+                Long skuId = cart.getSkuId();
 
-        orderConfirm.setOrderItems(carts.stream().map(cart -> {
-            OrderItemVO orderItemVO = new OrderItemVO();
-            Long skuId = cart.getSkuId();
-            SkuInfoEntity skuInfo = this.pmsClient.querySkuInfoBySkuId(skuId).getData();
-            if (skuInfo != null) {
-                orderItemVO.setSkuId(skuId);
-                orderItemVO.setWeight(skuInfo.getWeight());
-                orderItemVO.setCount(cart.getCount());
-                orderItemVO.setDefaultImage(skuInfo.getSkuDefaultImg());
-                orderItemVO.setTitle(skuInfo.getSkuTitle());
-                orderItemVO.setPrice(skuInfo.getPrice());
-            }
-            List<SkuSaleAttrValueEntity> attrValueEntities = pmsClient.querySkuSaleAttrValueBySkuId(skuId).getData();
-            orderItemVO.setSaleAttrValue(attrValueEntities);
-            List<WareSkuEntity> wareSkuEntities = this.wmsClient.queryWareSkusBySkuId(skuId).getData();
-            if (!CollectionUtils.isEmpty(wareSkuEntities)){
-                orderItemVO.setStore(wareSkuEntities.stream().anyMatch(wareSkuEntity -> wareSkuEntity.getStock() > 0));
-            }
-            return orderItemVO;
-        }).collect(Collectors.toList()));
+                //线程池异步请求，不返回参数
+                CompletableFuture<Void> skuInfoCompletableFuture = CompletableFuture.runAsync(() -> {
+                    SkuInfoEntity skuInfo = this.pmsClient.querySkuInfoBySkuId(skuId).getData();
+                    if (skuInfo != null) {
+                        orderItemVO.setSkuId(skuId);
+                        orderItemVO.setWeight(skuInfo.getWeight());
+                        orderItemVO.setCount(cart.getCount());
+                        orderItemVO.setDefaultImage(skuInfo.getSkuDefaultImg());
+                        orderItemVO.setTitle(skuInfo.getSkuTitle());
+                        orderItemVO.setPrice(skuInfo.getPrice());
+                    }
+                }, threadPoolExecutor);
+
+                //线程池异步请求，不返回参数
+                CompletableFuture<Void> saleAttrCompletableFuture = CompletableFuture.runAsync(() -> {
+                    List<SkuSaleAttrValueEntity> attrValueEntities = pmsClient.querySkuSaleAttrValueBySkuId(skuId).getData();
+                    orderItemVO.setSaleAttrValue(attrValueEntities);
+                }, threadPoolExecutor);
+
+                //线程池异步请求，不返回参数
+                CompletableFuture<Void> wareCompletableFuture = CompletableFuture.runAsync(() -> {
+                    List<WareSkuEntity> wareSkuEntities = this.wmsClient.queryWareSkusBySkuId(skuId).getData();
+                    if (!CollectionUtils.isEmpty(wareSkuEntities)) {
+                        orderItemVO.setStore(wareSkuEntities.stream().anyMatch(wareSkuEntity -> wareSkuEntity.getStock() > 0));
+                    }
+                }, threadPoolExecutor);
+
+                CompletableFuture.allOf(skuInfoCompletableFuture, saleAttrCompletableFuture, wareCompletableFuture).join();
+                return orderItemVO;
+            }).collect(Collectors.toList()));
+        });
+
 
         //查询用户信息，获取积分
-        MemberEntity memberEntity = this.umsClient.queryMemberByUserId(userId).getData();
-        orderConfirm.setBounds(memberEntity.getIntegration());
+        CompletableFuture<Void> memberCompletableFuture = CompletableFuture.runAsync(() -> {
+            MemberEntity memberEntity = this.umsClient.queryMemberByUserId(userId).getData();
+            orderConfirm.setBounds(memberEntity.getIntegration());
+        }, threadPoolExecutor);
 
         //生成一个唯一标志，防止重复提交,(页面一份，Redis一份)
-        String idStr = IdWorker.getIdStr();
-        orderConfirm.setOrderToken(idStr);
+        CompletableFuture<Void> tokenCompletableFuture = CompletableFuture.runAsync(() -> {
+            String idStr = IdWorker.getIdStr();
+            orderConfirm.setOrderToken(idStr);
+        }, threadPoolExecutor);
+
+        CompletableFuture.allOf(addressCompletableFuture, skuCompletableFuture, tokenCompletableFuture,memberCompletableFuture).join();
 
         return orderConfirm;
     }
